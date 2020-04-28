8c0f49dd86cfd1d4b5a291b557dd516f122df6ab
Visakh Vijayan <visakh.vijayan@seenit.in>
2020-04-29 00:59:26 +0530

    New ES Index

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export default
 		elastic: 
 		{
 			local: "http://localhost:9200/",
-			bonsai: "https://w66r0wadfy:eap8l8xqls@news-testing-9064294821.ap-southeast-2.bonsaisearch.net:443/"
+			bonsai: "https://k1z6usc4fw:96ifwkyrqn@news-testing-4794806055.ap-southeast-2.bonsaisearch.net:443/"
 		}
 	},
 	fcm:
